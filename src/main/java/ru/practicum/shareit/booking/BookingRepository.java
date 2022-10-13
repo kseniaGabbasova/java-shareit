e7deb57ca14aa2c67fa4440fca93122a56d7511b
kseniagabbasova <ksgabbasova@yandex.com>
2022-10-13 05:39:04 +0300

    add-bookings

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -39,7 +39,8 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
             "SELECT * " +
             "FROM bookings " +
             "WHERE booker_id = ?1 " +
-            "AND start_date_time >?2",
+            "AND start_date_time >?2 " +
+            "ORDER BY start_date_time DESC ",
             nativeQuery = true)
     List<Booking> findFutureByBooker(Integer bookerId, LocalDateTime localDateTime);
 
@@ -84,7 +85,8 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
             "FROM bookings AS b " +
             "LEFT JOIN items AS i ON i.item_id = b.item_id " +
             "WHERE i.owner_id = ?1 " +
-            "AND start_date_time >?2",
+            "AND start_date_time >?2 " +
+            "ORDER BY start_date_time DESC ",
             nativeQuery = true)
     List<Booking> findFutureByOwnerId(Integer ownerId, LocalDateTime localDateTime);
 
